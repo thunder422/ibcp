0364f539c3b2183fc0b2c0350cebb64a1d0fef66
Thunder422 <thunder422@verizon.net>
2010-01-31 02:21:12 +0000

    modified list class functions to be more 'list' like

--- list.h
+++ list.h
@@ -2,7 +2,7 @@
 //
 //	Interactive BASIC Compiler Project
 //	File: list.h - contains template for list generic list class
-//	Copyright (C) 2009  Thunder422
+//	Copyright (C) 2009-2010  Thunder422
 //
 //	This program is free software: you can redistribute it and/or modify
 //	it under the terms of the GNU General Public License as published by
@@ -22,6 +22,15 @@
 //
 //  2009-12-28  initial version
 //
+//  2010-01-30  added more "list" like functions;
+//              made stack calls inline to list functions;
+//              removed partial support for simple type list (values to
+//                append/push and remove/pop can be done by pointer) to reduce
+//                the number of functions;
+//              changed first() to return report;
+//              renamed top() to last();
+//              implemented top()
+
 
 #ifndef LIST_H
 #define LIST_H
@@ -70,35 +79,93 @@ public:
 	{
 		return master->prev == master;
 	}
-	// function get a reference to the current top (end) of list
-	Element *top(void)
+
+	// function get pointer to first element of list
+	Element *first(void)
+	{
+		return master->next;
+	}
+
+	// function get pointer to the last element of list
+	Element *last(void)
 	{
 		return master->prev;
 	}
 
-	// function to push (append) a constant generic value to end of list
-	void push(T value);
-	// function to pop (remove) a generic value from end of list
-	T pop(void);
-
-	// function to push (append) a generic value to end of list
-	void push(T *value);
-	// function to pop (remove) a generic value from end of list
-	bool pop(T *value);
-
-	// the following functions are used to scan a list from the beginning
-	// to the end using a for statement, for example:
+	// 2010-01-29: added list-like functions
 	//
-	//      List<int> int_stack;
-	//      List<int>element *element;
-	//      for (int_stack.first(element); int_stack.not_end(element);
-	//          int_stack.next(element)) {...}
+	// function to append a generic value to the end of list
+	Element *append(T *value);
 
-	// function to set element pointer to first item (bottom) of list
-	void first(Element *&element)
+	// function to append a generic value after an element
+	Element *append(Element *element, T *value);
+
+	// function to insert a generic value to begin of list
+	Element *insert(T *value);
+
+	// function to insert a generic value before an element
+	Element *insert(Element *element, T *value);
+
+	// function to remove a generic value from the end of list
+	bool remove(T *value);
+
+	// function to remove a generic value at an element
+	bool remove(Element *element, T *value);
+
+	// 2010-01-29: made stack functions inline calls to list functions
+	//
+	// function to push a generic value on the top of a stack
+	void push(T *value)
 	{
-		element = master->next;
+		append(value);
 	}
+
+	// function to push a constant value on the top of a stack
+	void push(T value)
+	{
+		append(&value);
+	}
+
+	// function to pop a generic value from top of a stack
+	bool pop(T *value)
+	{
+		return remove(value);
+	}
+
+	// function to pop a constant generic value from top of a stack
+	T pop(void)
+	{
+		T value;
+		remove(&value);
+		return value;
+	}
+
+	// function to get the generic value from top of a stack
+	bool top(T *value)
+	{
+		if (empty())
+		{
+			return false;
+		}
+		*value = master->prev->value;
+		return true;
+	}
+
+	// function to get the generic value from top of stack
+	T top(void)
+	{
+		// must have a value on the stack
+		return master->prev->value;
+	}
+
+	// the following functions are used to scan a list from the
+	// beginning to the end using a for statement, for example:
+	//
+	//      List<int> int_list;
+	//      List<int>Element *element;
+	//      for (element = int_list.first(); int_list.not_end(element);
+	//          int_list.next(element)) {...}
+
 	// function to set element pointer to the next item of list
 	void next(Element *&element)
 	{
@@ -112,46 +179,88 @@ public:
 };
 
 
-// This function pushes a value passes by value to the top of the stack
-// (appends to the end of the list).  This function is meant to be used
-// for lists containing simple type (e.g. int, double, etc.).  A new
-// element is allocated and is linked in to the end of th list.
-template <class T> void List<T>::push(T value)
-{
-	Element *element = new Element;
-	element->next = master;
-	element->prev = master->prev;
-	element->value = value;
-	(master->prev)->next = element;
-	master->prev = element;
-}
+// This function appends a value passed as a pointer to the end of the
+// list.  A new element is allocated and is linked in after the element
+// argument in the list.  A pointer to the new element is returned.
 
-template <class T> T List<T>::pop(void)
-{
-	Element *element = master->prev;
-	T value = element->value;
-	(element->prev)->next = element->next;
-	master->prev = element->prev;
-	delete element;
-	return value;
-}
-
-
-// This function pushes a value passes as a pointer to the top of the
-// stack (appends to the end of the list).  This function is meant to be
-// used for lists containing structures.  A new element is allocated and
-// is linked in to the end of th list.
-template <class T> void List<T>::push(T *value)
+// 2010-01-29: renamed from push, added return value
+template <class T> typename List<T>::Element *List<T>::append(T *value)
 {
 	Element *element = new Element;
 	element->next = master;
 	element->prev = master->prev;
 	element->value = *value;
-	(master->prev)->next = element;
+	master->prev->next = element;
 	master->prev = element;
+	return element;
 }
 
-template <class T> bool List<T>::pop(T *value)
+
+// This function appends a value passed as a pointer after the element
+// argument.  A new element is allocated and is linked in after the
+// element argument in the list.  A pointer to the new element is
+// returned.
+
+// 2010-01-29: renamed from push, added return value
+template <class T>
+typename List<T>::Element *List<T>::append(List<T>::Element *element, T *value)
+{
+	Element *new_elem = new Element;
+	new_elem->next = element->next;
+	new_elem->prev = element;
+	new_elem->value = *value;
+	element->next = new_elem;
+	new_elem->next->prev = new_elem;
+	return new_elem;
+}
+
+
+// This function inserts a value passed as a pointer to the beginning of
+// the list.  A new element is allocated and is linked at the beginning
+// of the list (after the master element).  A pointer to the new element
+// is returned.
+
+template <class T> typename List<T>::Element *List<T>::insert(T *value)
+{
+	Element *new_elem = new Element;
+	new_elem->next = master->next;
+	new_elem->prev = master;
+	new_elem->value = *value;
+	master->next = new_elem;
+	new_elem->next->prev = new_elem;
+	return new_elem;
+}
+
+
+// This function inserts a value passed as a pointer before the element
+// argument.  A new element is allocated and is linked in before the
+// element argument in the list.  A pointer to the new element is
+// returned.
+
+template <class T>
+typename List<T>::Element *List<T>::insert(List<T>::Element *element, T *value)
+{
+	Element *new_elem = new Element;
+	new_elem->next = element;
+	new_elem->prev = element->prev;
+	new_elem->value = *value;
+	element->prev = new_elem;
+	new_elem->prev->next = new_elem;
+	return new_elem;
+}
+
+
+// This function removes a value from the end of the list.  The function
+// is called with a pointer to the value in which to place (copy) the
+// value that is at the end of the list.
+//
+// If this function is called and the list empty, a false is returned
+// and nothing is copied.  The value is copied from the element at the
+// end of the list, which is de-linked from the list and deallocated.  A
+// true value is then returned.
+
+// 2010-01-29: renamed from pop
+template <class T> bool List<T>::remove(T *value)
 {
 	if (empty())
 	{
@@ -159,11 +268,32 @@ template <class T> bool List<T>::pop(T *value)
 	}
 	Element *element = master->prev;
 	*value = element->value;
-	(element->prev)->next = element->next;
+	element->prev->next = element->next;
 	master->prev = element->prev;
 	delete element;
 	return true;
 }
+
+
+// This function removes a value pointer to by the element argument.
+// The function is called with a pointer to the value in which to place
+// (copy) the value that is at the element argument.  After the value is
+// copied from the element argument, the element is de-linked from the
+// list and deallocated.  Returns whether the list still contains
+// elements after the element argument is removed.
+
+template <class T> bool List<T>::remove(Element *element, T *value)
+{
+	*value = element->value;
+	element->prev->next = element->next;
+	element->next->prev = element->prev;
+	delete element;
+	return !empty();
+}
+
+
+// This destructor function deallocates any elements remaing in the list
+// and then deallocates the master element.
 
 template <class T> List<T>::~List()
 {
@@ -173,7 +303,9 @@ template <class T> List<T>::~List()
 		delete element;
 		element = element->next;
 	}
-	delete master;
+	// 2010-01-30: must use delete[] since new[] was used to allocate
+	delete[] (char *)master;
 }
+
 
 #endif  // LIST_H
