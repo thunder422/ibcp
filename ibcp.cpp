0b61aaf12403e708128a8d0ae5f4bbd3e01801c6
Thunder422 <thunder422@verizon.net>
2010-03-14 05:04:35 +0000

    initial version; main source file for ibcp.exe program
    created from parts of test_parser.cpp source file

---
+++ ibcp.cpp
@@ -0,0 +1,122 @@
+// vim:ts=4:sw=4:
+//
+//	Interactive BASIC Compiler Project
+//	File: ibcp.h - miscellaneous functions
+//	Copyright (C) 2010  Thunder422
+//
+//	This program is free software: you can redistribute it and/or modify
+//	it under the terms of the GNU General Public License as published by
+//	the Free Software Foundation, either version 3 of the License, or
+//	(at your option) any later version.
+//
+//	This program is distributed in the hope that it will be useful,
+//	but WITHOUT ANY WARRANTY; without even the implied warranty of
+//	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//	GNU General Public License for more details.
+//
+//	For a copy of the GNU General Public License,
+//	see <http://www.gnu.org/licenses/>.
+// 
+//
+//  Change History:
+//
+//  2010-03-13  initial release, created to hold variables and functions
+//              related to definitions in the ibcp.h definitions file that have
+//              no other file into; and main, which includes the GPL header
+//              output, Token initialization call, Table initialization code,
+//              and a call to the test_parser() function
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "ibcp.h"
+
+void print_gpl_header(char *name)
+{
+    printf("\n%s  Copyright (C) 2010  Thunder422\n", name);
+    printf("This program comes with ABSOLUTELY NO WARRANTY.\n");
+    printf("This is free software, and you are welcome to\n");
+    printf("redistribute it under certain conditions.\n\n");
+}
+
+
+// Token
+bool Token::paren[sizeof_TokenType];
+bool Token::op[sizeof_TokenType];
+
+void Token::initialize(void)
+{
+	// set true for types that contain an opening parentheses
+	paren[IntFuncP_TokenType] = true;
+	paren[DefFuncP_TokenType] = true;
+	paren[Paren_TokenType] = true;
+
+	// set true for types that are considered an operator
+	op[Command_TokenType] = true;
+	op[Operator_TokenType] = true;
+}
+
+
+int test_parser(Parser &parser, Table *table, int argc, char *argv[]);
+
+
+int main(int argc, char *argv[])
+{
+	print_gpl_header(strrchr(argv[0], '\\') + 1);
+	
+	Token::initialize();
+	Table *table;
+	try
+	{
+		table = new Table();
+	}
+	catch (List<TableError> *error_list)
+	{
+		TableError error;
+
+		fprintf(stderr, "Error(s) found in Table:\n");
+		int n = 0;
+		bool more;
+		do
+		{
+			more = error_list->remove(NULL, &error);
+			fprintf(stderr, "Error #%d: ", ++n);
+			switch (error.type)
+			{
+			case Duplicate_TableErrType:
+				fprintf(stderr, "Code %d in table more than once "
+					"at entries %d and %d\n", error.duplicate.code,
+					error.duplicate.ifirst, error.duplicate.idup);
+				break;
+			case Missing_TableErrType:
+				fprintf(stderr, "Code %d missing from table\n",
+					error.missing.code);
+				break;
+			case Range_TableErrType:
+				fprintf(stderr, "Search type %d indexes (%d, %d) not correct\n",
+					error.range.type, error.range.ibeg, error.range.iend);
+				break;
+			case Overlap_TableErrType:
+				fprintf(stderr, "Search type %d indexes (%d, %d) overlap with "
+					"search type %d\n", error.overlap.type1, error.overlap.ibeg,
+					error.overlap.iend, error.overlap.type2);
+				break;
+			default:
+				fprintf(stderr, "Unknown error %d\n", error.type);
+				break;
+			}
+		}
+		while (more);
+		fprintf(stderr, "Program aborting!\n");
+		exit(1);
+	}
+	printf("Table initialization successful.\n");
+		
+	Parser parser(table);
+
+	return test_parser(parser, table, argc, argv);
+}
+	
+
+// end: ibcp.cpp
